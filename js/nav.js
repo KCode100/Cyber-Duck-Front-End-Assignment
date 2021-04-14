a0ed66adca28583e5998821632d68c8b97adfdcf
Kivi Corn <magiciankivi@gmail.com>
2021-04-14 16:15:08 +0300

    toggle menu image

--- js/nav.js
+++ js/nav.js
@@ -2,21 +2,8 @@ const menuEl = document.querySelector('.togglers__menu');
 const navigation = document.querySelector('.navigation__list');
 
 function toggle() {
-    const menuImg = 'http://127.0.0.1:5500/images/icon-menu.png';
-    const closeImg = 'http://127.0.0.1:5500/images/icon-close.png';
-    const currImg = menuEl.src;
-
-    menuEl.classList.toggle('close-icon')
-
-    if(currImg === menuImg) {
-        menuEl.setAttribute('src', closeImg);
-        navigation.style.height = "auto";
-        navigation.style.paddingBottom = "1rem";
-    } else {
-        menuEl.setAttribute('src', menuImg);
-        navigation.style.height = "0px";
-        navigation.style.paddingBottom = "0";
-    }
+    menuEl.classList.toggle('togglers__menu--close')
+    navigation.classList.toggle('nav-closed');
 }
 
 menuEl.addEventListener('click', toggle);
